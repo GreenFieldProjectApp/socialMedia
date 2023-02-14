d29886334d88c76aefc83fd77e2f95f31aa4e114
sellamihoussem <sellami.houssem1@gmail.com>
2023-02-14 15:05:14 +0100

    ORM finished

--- orm/user.model.js
+++ orm/user.model.js
@@ -1,12 +1,33 @@
 // Define and export the sequelize model that represents the table users.
 
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define("users", {
-    username: {
+  const User = sequelize.define("user", {
+    fullNme: {
+      type: DataTypes.STRING,
+      allowNull: false,
+   
+    },
+    email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
+    }, 
+     picture: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    
+    }, 
+     phoneNumber: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true,
+    }, 
+     password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      
     },
+
   });
 
   return User;
