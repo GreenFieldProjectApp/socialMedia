ad1c4493fbca9887de9e4f6234264e9d445df7c2
sellamihoussem <sellami.houssem1@gmail.com>
2023-02-16 10:34:28 +0100

    client

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 
 //Setting the port and listening for connections
-const port = 3000;
+const port = 3009;
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
